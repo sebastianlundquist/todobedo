ff1695f72d719c4b849dc2f56b1f3601521310fe
Sebastian Lundquist <sebastian.lundquist@gmail.com>
2019-09-08 15:04:46 +0200

    Change tasks list into a stateful widget

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:todobedo/widgets/task_tile.dart';
+import 'package:todobedo/models/task.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  @override
+  _TasksListState createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(name: 'Buy milk'),
+    Task(name: 'Buy eggs'),
+    Task(name: 'Buy bread'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        TasksTile(),
-        TasksTile(),
-        TasksTile(),
-      ],
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return TaskTile(
+            title: tasks[index].name,
+            isChecked: tasks[index].isDone,
+            checkboxCallback: (checkboxState) {
+              setState(() {
+                tasks[index].toggleDone();
+              });
+            });
+      },
+      itemCount: tasks.length,
     );
   }
 }
